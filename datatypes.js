be69c1e7288d4b6a95594ba4836b43c6a2d8b688
Max Irwin <max_irwin@yahoo.com>
2014-04-14 11:16:54 +0100

    Datatype validations complete

--- datatypes.js
+++ datatypes.js
@@ -20,59 +20,12 @@ datatypes.push({
 	validate:function(val) { return val===true || val===false || val==='on' || val==='checked' || val == '1' || val =='true' || val == '0' || val == 'false' || val == 1 || val == 0;}
 });
 
+//INTEGER NUMBER TYPES
+
 datatypes.push({
 	name:"byte",
 	cast:function(val){ return parseInt(val);},
-	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=0 && val<=256; }
-});
-
-datatypes.push({
-	name:"datetime",
-	cast:function(val){ return moment(val) },
-	validate:function(val) { return moment(val).isValid(); }
-});
-
-datatypes.push({
-	name:"decimal",
-	cast:function(val){ return parseFloat(val);},
-	validate:function(val) { return !isNaN(parseFloat(val)); }
-});
-
-datatypes.push({
-	name:"double",
-	cast:function(val){ return parseFloat(val);},
-	validate:function(val) { return !isNaN(parseFloat(val)); }
-});
-
-datatypes.push({
-	name:"single",
-	cast:function(val){ return parseFloat(val);},
-	validate:function(val) { return !isNaN(parseFloat(val)); }
-});
-
-
-datatypes.push({
-	name:"guid",
-	cast:function(val) { return val; },
-	validate:function(val) { var guidre = /^(guid\')?([\dabcdef]{8,8}-[\dabcdef]{4,4}-[\dabcdef]{4,4}-[\dabcdef]{4,4}-[\dabcdef]{12,12})(\')?$/i; return guidre.test(val); }
-});
-
-datatypes.push({
-	name:"int16",
-	cast:function(val){ return parseInt(val);},
-	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1; }
-});
-
-datatypes.push({
-	name:"int32",
-	cast:function(val){ return parseInt(val);},
-	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1; }
-});
-
-datatypes.push({
-	name:"int64",
-	cast:function(val){ return parseInt(val);},
-	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1; }
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=0 && val<256; }
 });
 
 datatypes.push({
@@ -82,44 +35,112 @@ datatypes.push({
 });
 
 datatypes.push({
+	name:"int16",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=-32768 && val<=32767; }
+});
+
+datatypes.push({
+	name:"uint16",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=0 && val<=65535; }
+});
+
+datatypes.push({
+	name:"int",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=-2147483648 && val<=2147483647; }
+});
+
+datatypes.push({
+	name:"int32",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=-2147483648 && val<=2147483647; }
+});
+
+datatypes.push({
+	name:"uint32",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=0 && val<=4294967295; }
+});
+
+datatypes.push({
+	name:"int64",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=-9223372036854775808 && val<=9223372036854775807; }
+});
+
+datatypes.push({
+	name:"uint64",
+	cast:function(val){ return parseInt(val);},
+	validate:function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=0 && val<=18446744073709551615; }
+});
+
+// FLOATING POINT NUMBER TYPES
+
+datatypes.push({
+	name:"decimal",
+	cast:function(val){ return parseFloat(val);},
+	validate:function(val) { var val = parseFloat(val); return !isNaN(val) && val>=(10e-28) && val<=(7.9*10e28); }
+});
+
+datatypes.push({
+	name:"double",
+	cast:function(val){ return parseFloat(val);},
+	validate:function(val) { var val = parseFloat(val); return !isNaN(val) && val>=(-1.79769313486232e308) && val<=(1.79769313486232e308); }
+});
+
+datatypes.push({
+	name:"float",
+	cast:function(val){ return parseFloat(val);},
+	validate:function(val) { var val = parseFloat(val); return !isNaN(val) && val>=(-3.402823e38) && val<=(3.402823e38); }
+});
+
+datatypes.push({
+	name:"single",
+	cast:function(val){ return parseFloat(val);},
+	validate:function(val) { var val = parseFloat(val); return !isNaN(val) && val>=(-3.402823e38) && val<=(3.402823e38); }
+});
+
+// STRING TYPES
+
+datatypes.push({
 	name:"string",
 	cast:function(val) { return val; },
 	validate:function(val) { return typeof val === "string"; }
 });
 
 datatypes.push({
+	name:"guid",
+	cast:function(val) { return val; },
+	validate:function(val) { var guidre = /^(guid\')?([\dabcdef]{8,8}-[\dabcdef]{4,4}-[\dabcdef]{4,4}-[\dabcdef]{4,4}-[\dabcdef]{12,12})(\')?$/i; return guidre.test(val); }
+});
+
+// DATE+TIME TYPES
+
+datatypes.push({
+	name:"date",
+	cast:function(val){ return moment(val) },
+	validate:function(val) { return moment(val).isValid(); }
+});
+
+datatypes.push({
+	name:"datetime",
+	cast:function(val){ return moment(val) },
+	validate:function(val) { return moment(val).isValid(); }
+});
+
+datatypes.push({
 	name:"time",
 	cast:function(val) { return val; },
-	validate:function(val) { return true; }
+	validate:function(val) { var timere = /^([\d]{1,2}):([\d]{2}):([\d]{2})(\.[\d]+)?$/; return timere.test(val); }
 });
 
 datatypes.push({
 	name:"datetimeoffset",
-	cast:function(val) { return val; },
-	validate:function(val) { return true; }
+	cast:function(val) { return moment(val); },
+	validate:function(val) { return moment(val).isValid(); }
 });
 
 
 module.exports = { defaults:datatypes };
-
-  
-/*
-//Declare all the oData Edm DataTypes
-var guidre = /^(guid\')?([\dabcdef]{8,8}-[\dabcdef]{4,4}-[\dabcdef]{4,4}-[\dabcdef]{4,4}-[\dabcdef]{12,12})(\')?$/i;
-new EdmType("NULL",		function(val) { return val===null || val == 'null'; }, function(val) { return null; });
-new EdmType("binary",	function(val) { return true; });
-new EdmType("boolean",	function(val) { return val===true || val===false || val==='on' || val==='checked' || val == '1' || val =='true' || val == '0' || val == 'false' || val == 1 || val == 0;}, function(val) { return (val===true || val==='on' || val==='checked' || val == '1' || val =='true' || val==1 )?1:0; });
-new EdmType("byte",		function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=0 && val<=256; },function(val){ return parseInt(val);});
-new EdmType("datetime",	function(val) { return (new Date(val))?true:false; },function(val){ return new Date(val) });
-new EdmType("decimal",	function(val) { return !isNaN(parseFloat(val)); }, function(val){ return parseFloat(val);}); //TODO - validate min/max
-new EdmType("double",	function(val) { return !isNaN(parseFloat(val)); }, function(val){ return parseFloat(val);}); //TODO - validate min/max
-new EdmType("single",	function(val) { return !isNaN(parseFloat(val)); }, function(val){ return parseFloat(val);}); //TODO - validate min/max
-new EdmType("guid",		function(val) { return guidre.test(val); });
-new EdmType("int16",	function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1; },function(val){ return parseInt(val);}); //TODO - validate min/max
-new EdmType("int32",	function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1; },function(val){ return parseInt(val);}); //TODO - validate min/max
-new EdmType("int64",	function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1; },function(val){ return parseInt(val);}); //TODO - validate min/max
-new EdmType("sbyte",	function(val) { var val = parseInt(val); return !isNaN(val) && val.toString().indexOf(".")===-1 && val>=-128 && val<=127; },function(val){ return parseInt(val);});
-new EdmType("string",	function(val) { return typeof val === "string"; });
-new EdmType("time",		function(val) { return true; }); //TODO - validate and cast
-new EdmType("datetimeoffset",function(val) { return true; }); //TODO - validate and cast
-*/
