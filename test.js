71d8ab8018b68ee90d89de4493bd489139facce5
Max Irwin <max_irwin@yahoo.com>
2014-07-31 21:13:01 +0100

    Test skeleton

---
+++ test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var fs = require('fs');
+var should = require('should');
+var heimdall = require('./heimdall');
+
+describe('Heimdall Tests',function() {
+
+	it('should ',function(){
+		
+	});
+
+});
